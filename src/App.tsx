3128b9fd92c6fc8f44ecbb00c2c9a56b2ca6c7f1
anara <tolybaianar05@gmail.com>
2025-06-10 17:31:12 +0500

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+import { Navbar } from "./components/Navbar";
 
-export function App  (){
-    return (
-        <Header/>
-        )
+export function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  );
 }
