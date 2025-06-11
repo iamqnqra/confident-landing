4b19c2c8400ea7c6936a4f08aae717ac2d0100e0
anara <tolybaianar05@gmail.com>
2025-06-11 17:39:04 +0500

    fix: rm div

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,20 +32,18 @@ export const Navbar = () => {
     <nav className={styles.nav}>
       <div className={styles.navContainer}>
         <div className={styles.navBody}>
-          <div className={styles.navRight}>
-            <ul className={styles.navElements}>
-              {LINKS.map((item) => (
-                <li key={item.href}>
-                  <a
-                    className={`${styles.navLink} ${styles.navLinkActive}`}
-                    href={item.href}
-                  >
-                    {item.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <ul className={styles.navElements}>
+            {LINKS.map((item) => (
+              <li key={item.href}>
+                <a
+                  className={`${styles.navLink} ${styles.navLinkActive}`}
+                  href={item.href}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
           <div className={styles.searchWrapper}>
             <input
               type="search"
