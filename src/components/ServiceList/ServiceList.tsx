35b91b08bdf14b1f30b1f6f74917fdb098e314ef
anarkaaa <231030@astanait.edu.kz>
2025-06-20 11:57:50 +0500

    chore: make a card as component

--- src/components/ServiceList/ServiceList.tsx
+++ src/components/ServiceList/ServiceList.tsx
@@ -1,5 +1,6 @@
 import styles from "./ServiceList.module.css";
 import Container from "../../shared/UIkit/Container/Container";
+import { ServiceCard } from "./ServiceListCard";
 
 export const ServiceList = () => {
   return (
@@ -16,134 +17,7 @@ export const ServiceList = () => {
             </h2>
           </div>
           <div className={styles.serviceGridCards}>
-            <div className={styles.serviceCardTop}>
-              <div className={styles.cardContent}>
-                <img
-                  src="./img/service/card1.svg"
-                  width={86}
-                  height={86}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <div className={styles.cardTextArea}>
-                  <h3 className={styles.serviceName}>lawn fertilization</h3>
-                  <p className={styles.serviceDescrip}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
-                  </p>
-                  <a href="#" target="true" className={styles.serviceLink}>
-                    learn More
-                    <img
-                      src="./img/about/arrow.svg"
-                      alt=""
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={styles.serviceCardTop}>
-              <div className={styles.cardContent}>
-                <img
-                  src="./img/service/card2.svg"
-                  width={86}
-                  height={86}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <div className={styles.cardTextArea}>
-                  <h3 className={styles.serviceName}>lawn fertilization</h3>
-                  <p className={styles.serviceDescrip}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore.
-                  </p>
-                  <a href="#" target="true" className={styles.serviceLink}>
-                    learn More
-                    <img
-                      src="./img/about/arrow.svg"
-                      alt=""
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.serviceCardBottom}>
-              <div className={styles.cardContent}>
-                <img
-                  src="./img/service/card3.svg"
-                  width={86}
-                  height={86}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <div className={styles.cardTextArea}>
-                  <h3 className={styles.serviceName}>lawn fertilization</h3>
-                  <p className={styles.serviceDescrip}>
-                    Lorem ipsum dolor sit amet, consectetur.
-                  </p>
-                  <a href="#" target="true" className={styles.serviceLink}>
-                    learn More
-                    <img
-                      src="./img/about/arrow.svg"
-                      alt=""
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={styles.serviceCardBottom}>
-              <div className={styles.cardContent}>
-                <img
-                  src="./img/service/card4.svg"
-                  width={86}
-                  height={86}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <div className={styles.cardTextArea}>
-                  <h3 className={styles.serviceName}>lawn fertilization</h3>
-                  <p className={styles.serviceDescrip}>
-                    Lorem ipsum dolor sit amet, consectetur.
-                  </p>
-                  <a href="#" target="true" className={styles.serviceLink}>
-                    learn More
-                    <img
-                      src="./img/about/arrow.svg"
-                      alt=""
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={styles.serviceCardBottom}>
-              <div className={styles.cardContent}>
-                <img
-                  src="./img/service/card5.svg"
-                  width={86}
-                  height={86}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <div className={styles.cardTextArea}>
-                  <h3 className={styles.serviceName}>lawn fertilization</h3>
-                  <p className={styles.serviceDescrip}>
-                    Lorem ipsum dolor sit amet, consectetur.
-                  </p>
-                  <a href="#" target="true" className={styles.serviceLink}>
-                    learn More
-                    <img
-                      src="./img/about/arrow.svg"
-                      alt=""
-                      aria-hidden="true"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <ServiceCard></ServiceCard>
           </div>
         </div>
       </Container>
---
+++ src/components/ServiceList/ServiceListCard.tsx
@@ -0,0 +1,69 @@
+import styles from "./ServiceListCard.module.css";
+
+const CardItem = [
+  {
+    imageSrc: "./img/service/card1.svg",
+    title: "lawn fertilization",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+    link: "#",
+    className: styles.serviceCardTop,
+  },
+  {
+    imageSrc: "./img/service/card2.svg",
+    title: "lawn fertilization",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+    link: "#",
+    className: styles.serviceCardTop,
+  },
+  {
+    imageSrc: "./img/service/card3.svg",
+    title: "lawn fertilization",
+    description: "Lorem ipsum dolor sit amet, consectetur.",
+    link: "#",
+    className: styles.serviceCardBottom,
+  },
+  {
+    imageSrc: "./img/service/card4.svg",
+    title: "lawn fertilization",
+    description: "Lorem ipsum dolor sit amet, consectetur.",
+    link: "#",
+    className: styles.serviceCardBottom,
+  },
+  {
+    imageSrc: "./img/service/card5.svg",
+    title: "lawn fertilization",
+    description: "Lorem ipsum dolor sit amet, consectetur.",
+    link: "#",
+    className: styles.serviceCardBottom,
+  },
+];
+
+export const ServiceCard = () => {
+  return (
+    <>
+      {CardItem.map((card, idx) => (
+        <div className={card.className} key={idx}>
+          <div className={styles.cardContent}>
+            <img
+              src={card.imageSrc}
+              width={86}
+              height={86}
+              alt=""
+              aria-hidden="true"
+            />
+            <div className={styles.cardTextArea}>
+              <h3 className={styles.serviceName}>{card.title}</h3>
+              <p className={styles.serviceDescrip}>{card.description}</p>
+              <a href={card.link} target="true" className={styles.serviceLink}>
+                learn More
+                <img src="./img/about/arrow.svg" alt="" aria-hidden="true" />
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
