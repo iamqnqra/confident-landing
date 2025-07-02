7a1a455b682ff68d87627990befa4551662a2ae4
anarkaaa <231030@astanait.edu.kz>
2025-07-02 17:33:07 +0500

    feat: update design od menu
    added transition, smooth appearance

--- src/shared/UIkit/MobileMenu/MobileMenu.tsx
+++ src/shared/UIkit/MobileMenu/MobileMenu.tsx
@@ -28,7 +28,6 @@ const MENU_ITEMS = [
 type MobileMenuProps = {
   className?: string;
 };
-
 export const MobileMenu = ({ className }: MobileMenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -37,78 +36,83 @@ export const MobileMenu = ({ className }: MobileMenuProps) => {
       <BurgerMenu onClick={() => setIsOpen(true)} />
 
       {isOpen && (
-        <div className={styles.overlay}>
-          <div className={styles.menu}>
-            <div className={styles.topBar}>
-              <img src="/img/header/logo.svg" alt="Logo" />
-              <button
-                className={styles.closeBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                ✕
-              </button>
-            </div>
+        <div className={styles.backdrop} onClick={() => setIsOpen(false)} />
+      )}
 
-            <nav className={styles.nav}>
-              {MENU_ITEMS.map((item) => (
-                <details key={item.title} className={styles.menuItem}>
-                  <summary className={styles.menuSummary}>
-                    {item.title}
-                    {item.children && <span className={styles.arrow}></span>}
-                  </summary>
-                  {item.children && (
-                    <div className={styles.subMenu}>
-                      {item.children.map((child) => (
-                        <a key={child} href="#" className={styles.subMenuLink}>
-                          {child}
-                        </a>
-                      ))}
-                    </div>
-                  )}
-                </details>
-              ))}
-            </nav>
+      <div
+        className={`${styles.overlay} ${isOpen ? styles.overlayActive : ""}`}
+      >
+        <div className={styles.menu}>
+          <div className={styles.topBar}>
+            <img src="/img/header/logo.svg" alt="Logo" />
+            <button
+              className={styles.closeBtn}
+              onClick={() => setIsOpen(false)}
+            >
+              ✕
+            </button>
+          </div>
 
-            <div className={styles.contact}>
-              <a
-                href="https://www.google.com/maps/@51.134464,71.3981952,14z?entry=ttu&g_ep=EgoyMDI1MDYwNC4wIKXMDSoASAFQAw%3D%3D"
-                className={styles.headerLocation}
-                target="_blank"
-              >
-                <img
-                  src="./img/header/location.svg"
-                  width={19}
-                  height={24}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <span className={styles.headerLocationMean}>
-                  {" "}
-                  St. Sanguin Number 40
-                </span>
-              </a>
-              <a
-                href="tel:+1234 - 4567 - 890"
-                className={styles.headerContactPhone}
-              >
-                <img
-                  src="./img/header/phone.svg"
-                  width={19}
-                  height={19}
-                  alt=""
-                  aria-hidden="true"
-                />
-                <span className={styles.headerPhoneNumber}>
-                  +1234 - 4567 - 890
-                </span>
-              </a>
-            </div>
-            <div className={styles.menuButton}>
-              <Button className={styles.appointmentButton}>Appointment</Button>
-            </div>
+          <nav className={styles.nav}>
+            {MENU_ITEMS.map((item) => (
+              <details key={item.title} className={styles.menuItem}>
+                <summary className={styles.menuSummary}>
+                  {item.title}
+                  {item.children && <span className={styles.arrow}></span>}
+                </summary>
+                {item.children && (
+                  <div className={styles.subMenu}>
+                    {item.children.map((child) => (
+                      <a key={child} href="#" className={styles.subMenuLink}>
+                        {child}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </details>
+            ))}
+          </nav>
+
+          <div className={styles.contact}>
+            <a
+              href="https://www.google.com/maps/@51.134464,71.3981952,14z?entry=ttu"
+              className={styles.headerLocation}
+              target="_blank"
+            >
+              <img
+                src="./img/header/location.svg"
+                width={19}
+                height={24}
+                alt=""
+                aria-hidden="true"
+              />
+              <span className={styles.headerLocationMean}>
+                {" "}
+                St. Sanguin Number 40
+              </span>
+            </a>
+            <a
+              href="tel:+1234 - 4567 - 890"
+              className={styles.headerContactPhone}
+            >
+              <img
+                src="./img/header/phone.svg"
+                width={19}
+                height={19}
+                alt=""
+                aria-hidden="true"
+              />
+              <span className={styles.headerPhoneNumber}>
+                +1234 - 4567 - 890
+              </span>
+            </a>
+          </div>
+
+          <div className={styles.menuButton}>
+            <Button className={styles.appointmentButton}>Appointment</Button>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
